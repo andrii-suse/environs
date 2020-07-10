a88c874a18688ec24b9b03422e693a52f0b1314c
Andrii Nikitin <anikitin@suse.de>
2020-07-10 11:29:55 +0200

    Allow custom MIRRORCACHE_ROOT

--- .product/mc/branch/backstage/job.sh
+++ .product/mc/branch/backstage/job.sh
@@ -6,7 +6,7 @@ set -e
 
 [ "$#" -ne 1 ] || extra=-e
 
-MIRRORCACHE_ROOT=__workdir/dt \
+MIRRORCACHE_ROOT=${MIRRORCACHE_ROOT:-__workdir/dt} \
 MIRRORCACHE_CITY_MMDB=__srcdir/t/data/city.mmdb \
 MOJO_LISTEN=http://127.0.0.1:${port} \
 __srcdir/script/mirrorcache minion job $extra "$@"
--- .product/mc/branch/backstage/shoot.sh
+++ .product/mc/branch/backstage/shoot.sh
@@ -4,7 +4,7 @@ set -e
     export TEST_PG=$(cat __workdir/TEST_PG)
 }
 
-MIRRORCACHE_ROOT=__workdir/dt \
+MIRRORCACHE_ROOT=${MIRRORCACHE_ROOT:-__workdir/dt} \
 MIRRORCACHE_CITY_MMDB=__srcdir/t/data/city.mmdb \
 MOJO_LISTEN=http://127.0.0.1:${port} \
 __srcdir/script/mirrorcache backstage run --oneshot
